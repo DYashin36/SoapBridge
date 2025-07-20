da3b5ac3f4741d766f349aeb2add220b7b179c2f
Dyashin <dyashin36@gmail.com>
2025-07-20 11:00:33 +0400

    <m:BiblRecords> add

--- simple-rest-wrapper/src/main/java/ru/omega/RestSoapBridge.java
+++ simple-rest-wrapper/src/main/java/ru/omega/RestSoapBridge.java
@@ -1,8 +1,13 @@
 package ru.omega;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import java.io.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class RestSoapBridge extends HttpServlet {
 
@@ -74,7 +79,9 @@ public class RestSoapBridge extends HttpServlet {
             "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
             "  <soap:Body>\n" +
             "    <GetShortRecordListResponse xmlns=\"http://www.omega-spb.ru/bibl\">\n" +
+            "<m:BiblRecords>"+
             recordXml +
+            "<m:BiblRecords>"+
             "    </GetShortRecordListResponse>\n" +
             "  </soap:Body>\n" +
             "</soap:Envelope>";
@@ -117,6 +124,60 @@ public class RestSoapBridge extends HttpServlet {
     //         "  </soap:Body>\n" +
     //         "</soap:Envelope>";
     // }
+
+    private String generateBiblRecordsResponse()
+    {
+        String exchangeXml
+                = "<ExchangeXML xmlns=\"http://www.imc-dspace-new.org\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
+                + "    <m:BiblRecords><Record>"
+                +"<Contributor><Qualifier>Department</Qualifier><Value/></Contributor>"
+                + "        <Contributor><Qualifier>Other</Qualifier><Value/></Contributor>"
+                + "        <Contributor><Qualifier>Subdepartment</Qualifier><Value/></Contributor>"
+                + "        <Date><Qualifier>Issued</Qualifier><Value>2006</Value></Date>"
+                + "        <Date><Qualifier>Issued</Qualifier><Value/></Date>"
+                + "        <Identifier><Qualifier>Citation</Qualifier><Value>Савицкая, Т.Е. Савицкая, Т.Е. \"Африка внутри нас\" : парадоксы современных процессов визуализации культуры // Обсерватория культуры. - 2006. - № 6. - Текст : непосредственный</Value></Identifier>"
+                + "        <Identifier><Qualifier>Identifier</Qualifier><Value>RU/IS/BASE/234616783</Value></Identifier>"
+                + "        <Identifier><Qualifier>ISBN</Qualifier><Value/></Identifier>"
+                + "        <Identifier><Qualifier>ISMN</Qualifier><Value/></Identifier>"
+                + "        <Identifier><Qualifier>ISSN</Qualifier><Value/></Identifier>"
+                + "        <Identifier><Qualifier>Nps</Qualifier><Value/></Identifier>"
+                + "        <Identifier><Qualifier>Orcid</Qualifier><Value/></Identifier>"
+                + "        <Description><Qualifier>Abstract</Qualifier><Value/></Description>"
+                + "        <Description><Qualifier>FirstPage</Qualifier><Value>30</Value></Description>"
+                + "        <Description><Qualifier>LastPage</Qualifier><Value>35</Value></Description>"
+                + "        <Format><Qualifier>Extent</Qualifier><Value/></Format>"
+                + "        <Format><Qualifier>Mimetype</Qualifier><Value>Text</Value></Format>"
+                + "        <Language><Qualifier>ISO</Qualifier><Value/></Language>"
+                + "        <Publisher><Qualifier/><Value/></Publisher>"
+                + "        <Relation><Qualifier>IsPartOf</Qualifier><Value>Обсерватория культуры</Value></Relation>"
+                + "        <Rights><Qualifier/><Value/></Rights>"
+                + "        <Rights><Qualifier>License</Qualifier><Value/></Rights>"
+                + "        <Rights><Qualifier>Url</Qualifier><Value/></Rights>"
+                + "        <Subject><Qualifier>RuBBK</Qualifier><Value/></Subject>"
+                + "        <Subject><Qualifier>RuGASNTI</Qualifier><Value/></Subject>"
+                + "        <Subject><Qualifier>Subject</Qualifier><Value>ПРЕПОДАВАНИЕ КУЛЬТУРОЛОГИИ</Value></Subject>"
+                + "        <Subject><Qualifier>Subject</Qualifier><Value>ВЫСШЕЕ ОБРАЗОВАНИЕ</Value></Subject>"
+                + "        <Subject><Qualifier>Subject</Qualifier><Value>ВЫСШАЯ ШКОЛА</Value></Subject>"
+                + "        <Subject><Qualifier>UDC</Qualifier><Value/></Subject>"
+                + "        <Title><Qualifier/><Value>\"Африка внутри нас\" : парадоксы современных процессов визуализации культуры</Value></Title>"
+                + "        <Title><Qualifier>Alternative</Qualifier><Value/></Title>"
+                + "        <Type><Qualifier/><Value>Text</Value></Type>"
+                + "        <Thesis><Qualifier>Level</Qualifier><Value/></Thesis>"
+                + "        <Thesis><Qualifier>Speciality</Qualifier><Value/></Thesis>"
+                + "        <Source><Qualifier/><Value>Обсерватория культуры. - 2006. -  № 6. - Текст  : непосредственный</Value></Source>"
+                + "        <Source><Qualifier/><Value/></Source>"
+                + "    </Record></m:BiblRecords>"
+                + "</ExchangeXML>";
+
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                + "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">"
+                + "<soap:Body>"
+                + "<GetRecordsInfoResponse xmlns=\"http://imc.parus-s.ru\">"
+                + exchangeXml
+                + "</GetRecordsInfoResponse>"
+                + "</soap:Body>"
+                + "</soap:Envelope>";
+    }
 
 
     private String generateErrorResponse()
