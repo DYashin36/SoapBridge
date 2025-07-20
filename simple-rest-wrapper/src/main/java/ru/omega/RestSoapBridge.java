49f286e884b3ff3cff89063b5f0b3103fd755a9b
Dyashin <dyashin36@gmail.com>
2025-07-20 11:26:55 +0400

    "m:" fix#2

--- simple-rest-wrapper/src/main/java/ru/omega/RestSoapBridge.java
+++ simple-rest-wrapper/src/main/java/ru/omega/RestSoapBridge.java
@@ -76,15 +76,15 @@ public class RestSoapBridge extends HttpServlet {
             "  </Records>\n";
 
         return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
-            "  <soap:Body>\n" +
-            "    <GetShortRecordListResponse xmlns=\"http://www.omega-spb.ru/bibl\">\n" +
-            "<m:BiblRecords>"+
-            recordXml +
-            "</m:BiblRecords>"+
-            "    </GetShortRecordListResponse>\n" +
-            "  </soap:Body>\n" +
-            "</soap:Envelope>";
+    "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
+    "  <soap:Body>\n" +
+    "    <GetShortRecordListResponse xmlns=\"http://www.omega-spb.ru/bibl\" xmlns:m=\"http://www.omega-spb.ru/bibl\">\n" +
+    "      <m:BiblRecords>\n" +
+             recordXml +
+    "      </m:BiblRecords>\n" +
+    "    </GetShortRecordListResponse>\n" +
+    "  </soap:Body>\n" +
+    "</soap:Envelope>";
     }
 
     // private String generateErrorResponse() {
